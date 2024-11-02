2e3e77e48915384078d44ff8cf62e7824e33ff09
ItzMeTheAcoustism <milicamartic2023@gmail.com>
2024-11-02 12:40:33 +0100

    pajton program za uspeh

---
+++ uspeh/uspeh.py
@@ -0,0 +1,11 @@
+prosek= float(input("unesite prosek ucenika"))
+if prosek > 4.5:
+    print("odlican")
+elif prosek > 3.5:
+    print("vrlo dobar")
+elif prosek > 2.5:
+    print("dobar")
+elif prosek > 1.5:
+    print("dovoljan")
+elif prosek < 1.5:
+    print("nedovoljan")
